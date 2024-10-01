eb547c1586e2d310e4525e5c3079ebe77fe07291
ICS4U-1-2024 <ics4u-1-2024@mths.ca>
2024-10-01 14:36:42 -0400

    init

--- Main.java
+++ Main.java
@@ -1,40 +1,54 @@
 /*
-* The ...
-*
-* @author  Mr Coxall
-* @version 1.0
-* @since   2020-01-01
-*/
+ * This program calculates the area of circle
+ *
+ * @author  Mr Coxall
+ * @version 1.0
+ * @since   2020-09-01
+ */
 
 import java.util.Scanner;
 
-/**
-* This ...
-*/
 final class Main {
-
     /**
-    * Prevent instantiation.
-    * Throw an exception IllegalStateException.
-    * if this is ever called
-    *
-    * @throws IllegalStateException if this is ever called
-    *
-    */
+     * Calculates calculates the area of circle.
+     *
+     * @param args nothing passed in
+     */
+    public static float calculateArea(int radius) {
+        // process
+        float area = Math.PI * Math.pow(radius, 2);
+
+        return area;
+    }
+
     private Main() {
+        // Prevent instantiation
+        // Optional: throw an exception e.g. AssertionError
+        // if this ever *is* called
         throw new IllegalStateException("Cannot be instantiated");
     }
 
     /**
-    * The starting main() function.
-    *
-    * @param args No args will be used
-    */
-    public static void main(final String[] args) {
-        // create a scanner object to get input
-        Scanner scanner = new Scanner(System.in);
+     * Main entry point into program.
+     *
+     * @param args nothing passed in
+     */
+    public static float main(final String[] args) {
+        int radius;
+        float area = 0.0;
 
-        scanner.close();
+        // input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius of a circle (cm): ");
+        radius = scanner.nextInt();
+        System.out.println();
+
+        // call functions
+        area = calculateArea(radius);
+
+        // output
+        System.out.printf("The area is %d cm²%n", area);
+
         System.out.println("\nDone.");
     }
 }
